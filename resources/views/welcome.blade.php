85c89a46a0f1c71583a981f07498d402c6b43a4c
nadim <boussarsarnadim@gmail.com>
2024-09-11 14:42:17 +0100

    version 1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>Site E-commerce</title>
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div>Bienvenue à mon notre site</div>
+    </body>
+</html>
